1410c7b3dad16dd66629f7b43068dd8b736c11ed
Anton Maminov <anton.linux@gmail.com>
2018-03-18 19:41:14 +0200

    use cache for index

--- src/crystal-shards.cr
+++ src/crystal-shards.cr
@@ -4,14 +4,17 @@ require "cache"
 
 require "./github"
 
-RECENTLY_CACHE = Cache::MemoryStore(String, String).new(expires_in: 30.minutes)
+REPOS_CACHE = Cache::MemoryStore(String, Github::Repos).new(expires_in: 30.minutes)
 
 get "/" do
-  client = HTTP::Client.new("api.github.com", 443, true)
-  client.basic_auth ENV["GITHUB_USER"], ENV["GITHUB_KEY"]
-  url = "/search/repositories?q=language=Crystal"
-  response = client.get(url)
-  repos = Github::Repos.from_json(response.body)
+  repos = REPOS_CACHE.fetch("repos") do
+    client = HTTP::Client.new("api.github.com", 443, true)
+    client.basic_auth ENV["GITHUB_USER"], ENV["GITHUB_KEY"]
+    url = "/search/repositories?q=language=Crystal"
+    response = client.get(url)
+
+    Github::Repos.from_json(response.body)
+  end
 
   render "src/views/index.slang", "src/views/layouts/layout.slang"
 end
