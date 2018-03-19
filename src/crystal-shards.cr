f307b416601d85a27eeb9ad5db0c0849d9a49a20
Anton Maminov <anton.linux@gmail.com>
2018-03-19 21:29:31 +0200

    migrate from MemoryStore to RedisStore

--- src/crystal-shards.cr
+++ src/crystal-shards.cr
@@ -6,20 +6,22 @@ require "emoji"
 
 require "./github"
 
-RECENTLY_CACHE = Cache::MemoryStore(String, Github::Repos).new(expires_in: 30.minutes)
-POPULAR_CACHE = Cache::MemoryStore(String, Github::Repos).new(expires_in: 30.minutes)
+RECENTLY_CACHE = Cache::RedisStore(String, String).new(expires_in: 30.minutes)
+POPULAR_CACHE = Cache::RedisStore(String, String).new(expires_in: 30.minutes)
 
 get "/" do
   recently_repos = RECENTLY_CACHE.fetch("repos") do
     github_client = Github::API.new(ENV["GITHUB_USER"], ENV["GITHUB_KEY"])
-    github_client.recently_updated
+    github_client.recently_updated.to_json
   end
 
   popular_repos = POPULAR_CACHE.fetch("repos") do
     github_client = Github::API.new(ENV["GITHUB_USER"], ENV["GITHUB_KEY"])
-    github_client.popular
+    github_client.popular.to_json
   end
 
+  recently_repos = Github::Repos.from_json(recently_repos)
+  popular_repos = Github::Repos.from_json(popular_repos)
 
   render "src/views/index.slang", "src/views/layouts/layout.slang"
 end
