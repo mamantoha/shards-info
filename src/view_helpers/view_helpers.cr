94b66305beb9cb5142ec285f959cee6e5d74f583
Anton Maminov <anton.linux@gmail.com>
2018-07-10 14:25:51 +0300

    rename @repos to @collection

--- src/view_helpers/view_helpers.cr
+++ src/view_helpers/view_helpers.cr
@@ -1,7 +1,7 @@
 require "ecr"
 
 module ViewHelpers
-  record GithubPaginator, repos : Github::Iterable, page : Int32, link : String do
+  record GithubPaginator, collection : Github::Iterable, page : Int32, link : String do
     ECR.def_to_s "#{__DIR__}/paginate_github.ecr"
   end
 end
