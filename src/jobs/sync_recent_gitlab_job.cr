2ec688b8a8ae64c56137d23d4ee9be1ddac2510a
Anton Maminov <anton.linux@gmail.com>
2020-01-26 17:05:46 +0200

    improve sync_recent_gitlab_job

--- src/jobs/sync_recent_gitlab_job.cr
+++ src/jobs/sync_recent_gitlab_job.cr
@@ -4,22 +4,28 @@ class SyncRecentGitlabJob < Mosquito::PeriodicJob
   run_every 30.minutes
 
   def perform
-    gitlab_client = Gitlab::API.new(ENV["GITLAB_ACCESS_TOKEN"])
-
     projects = gitlab_client.recently_updated
 
     projects.each do |_gitlab_project|
-      gitlab_project = gitlab_client.project(_gitlab_project.id)
-
-      if repository = Repository.query.find({provider: "gitlab", provider_id: gitlab_project.id})
+      if repository = Repository.query.find({provider: "gitlab", provider_id: _gitlab_project.id})
         # Update if repository has been changed
-        if repository.last_activity_at != gitlab_project.last_activity_at
-          GitlabHelpers.sync_project(gitlab_project)
+        if repository.last_activity_at != _gitlab_project.last_activity_at
+          sync_project(_gitlab_project)
         end
       else
         # Create new repository
-        GitlabHelpers.sync_project(gitlab_project)
+        sync_project(_gitlab_project)
       end
     end
+  end
+
+  private def gitlab_client
+    @gilab_client ||= Gitlab::API.new(ENV["GITLAB_ACCESS_TOKEN"])
+  end
+
+  private def sync_project(_gitlab_project : Gitlab::Project)
+    gitlab_project = gitlab_client.project(_gitlab_project.id)
+
+    GitlabHelpers.sync_project(gitlab_project)
   end
 end
