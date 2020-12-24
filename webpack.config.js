28dbcf3dacff4a56f9de62e211d30412d99e0bd2
Anton Maminov <anton.linux@gmail.com>
2020-12-24 02:04:38 +0200

    update all npm packages to latest versions

--- webpack.config.js
+++ webpack.config.js
@@ -21,17 +21,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: require.resolve('jquery'),
-        use: [
-          {
-            loader: 'expose-loader',
-            options: 'jQuery'
-          },
-          {
-            loader: 'expose-loader',
-            options: '$'
-          }
-        ]
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
+        options: {
+          exposes: ["$", "jQuery"],
+        },
       },
       {
         test: /\.(sa|sc|c)ss$/i,
