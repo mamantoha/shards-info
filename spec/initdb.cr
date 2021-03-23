6f461fabf89c7c297ca13d77a63afff5b6e5d737
Anton Maminov <anton.linux@gmail.com>
2021-03-23 18:56:19 +0200

    fix

--- spec/initdb.cr
+++ spec/initdb.cr
@@ -1,5 +1,5 @@
 require "clear"
-require "../config/config"
+require "../src/db/migrations/**"
 
 def initdb
   pg.exec("DROP DATABASE IF EXISTS shards_info_test;")
@@ -11,11 +11,27 @@ def initdb
 end
 
 def database_url
-  ENV["DATABASE_URL"]? || "postgres://postgres@localhost/shards_info_test"
+  ENV["DATABASE_URL"]? || "postgres://postgres:postgres@localhost/shards_info_test"
+end
+
+def postgres_user
+  ENV["POSTGRES_USER"]? || "postgres"
+end
+
+def postgres_password
+  ENV["POSTGRES_PASSWORD"]? || ""
+end
+
+def postgres_host
+  ENV["POSTGRES_HOST"]? || "localhost"
+end
+
+def postgres_db
+  ENV["POSTGRES_DB"]? || "postgres"
 end
 
 def pg
-  DB.open(database_url)
+  DB.open("postgres://#{postgres_user}:#{postgres_password}@#{postgres_host}/#{postgres_db}")
 end
 
 Log.setup(:debug)
