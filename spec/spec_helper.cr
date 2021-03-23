c433b6ecde5e68f31458254e351ec05ce9c6f5c5
Anton Maminov <anton.linux@gmail.com>
2021-03-23 17:14:12 +0200

    fix

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,3 +1,30 @@
 require "spec"
 require "spec-kemal"
 require "../src/web"
+
+def initdb
+  pg.exec("DROP DATABASE IF EXISTS shards_info_test;")
+  pg.exec("CREATE DATABASE shards_info_test;")
+end
+
+def postgres_user
+  ENV["POSTGRES_USER"]? || "postgres"
+end
+
+def postgres_password
+  ENV["POSTGRES_PASSWORD"]? || ""
+end
+
+def postgres_host
+  ENV["POSTGRES_HOST"]? || "localhost"
+end
+
+def postgres_db
+  ENV["POSTGRES_DB"]? || "postgres"
+end
+
+def pg
+  DB.open("postgres://#{postgres_user}:#{postgres_password}@#{postgres_host}/#{postgres_db}")
+end
+
+initdb
