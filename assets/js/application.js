3dfa2cc8040dfd8f70efebbf553497d3ff7c0566
Anton Maminov <anton.linux@gmail.com>
2021-05-12 16:58:04 +0300

    format js and css

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,13 +1,13 @@
-document.addEventListener("turbolinks:load", function() {
+document.addEventListener("turbolinks:load", function () {
   if (typeof ga === "function") {
     ga("set", "location", event.data.url);
     ga("send", "pageview");
-  };
+  }
 
-  $('form#search').on('submit', function(e) {
+  $("form#search").on("submit", function (e) {
     e.preventDefault();
     query = $(e.target).find("input[name='query']").val();
-    query = query.replace(/\s/g, '+');
+    query = query.replace(/\s/g, "+");
     Turbolinks.visit("/search?query=" + query);
   });
 
@@ -16,43 +16,43 @@ document.addEventListener("turbolinks:load", function() {
 
   $(function () {
     var hash = window.location.hash;
-    hash && $('.nav a[href="' + hash + '"]').tab('show');
+    hash && $('.nav a[href="' + hash + '"]').tab("show");
 
-    $('.nav-tabs a').click(function (e) {
-      $(this).tab('show');
-      var scrollmem = $('body').scrollTop();
+    $(".nav-tabs a").click(function (e) {
+      $(this).tab("show");
+      var scrollmem = $("body").scrollTop();
       window.location.replace(this.hash);
-      history.replaceState({ turbolinks: {} }, '');
-      $('html,body').scrollTop(scrollmem);
+      history.replaceState({ turbolinks: {} }, "");
+      $("html,body").scrollTop(scrollmem);
     });
 
     // Back To Top Button
-    if ($('#back-to-top').length) {
+    if ($("#back-to-top").length) {
       var scrollTrigger = 100, // px
         backToTop = function () {
           var scrollTop = $(window).scrollTop();
           if (scrollTop > scrollTrigger) {
-            $('#back-to-top').addClass('show');
+            $("#back-to-top").addClass("show");
           } else {
-            $('#back-to-top').removeClass('show');
+            $("#back-to-top").removeClass("show");
           }
         };
       backToTop();
-      $(window).on('scroll', function () {
+      $(window).on("scroll", function () {
         backToTop();
       });
     }
 
-    Turbolinks.setProgressBarDelay(200)
+    Turbolinks.setProgressBarDelay(200);
   });
 
   var moveTo = new MoveTo();
-  var trigger = $("#back-to-top")
+  var trigger = $("#back-to-top");
   moveTo.registerTrigger(trigger[0]);
 
-  pagesCount = $('#pagination').data("pagesCount");
-  currentPage = $('#pagination').data("currentPage");
-  var $pagination = $('#pagination');
+  pagesCount = $("#pagination").data("pagesCount");
+  currentPage = $("#pagination").data("currentPage");
+  var $pagination = $("#pagination");
 
   $pagination.twbsPagination({
     totalPages: pagesCount,
@@ -64,7 +64,6 @@ document.addEventListener("turbolinks:load", function() {
     next: "&raquo;",
     first: "",
     last: "",
-    onPageClick: function (event, page) {
-    }
+    onPageClick: function (event, page) {},
   });
-})
+});
--- assets/main.js
+++ assets/main.js
@@ -1,24 +1,24 @@
-import $ from 'jquery'
-import jQuery from 'jquery'
+import $ from "jquery";
+import jQuery from "jquery";
 
-import 'bootstrap/js/dist/modal'
-import 'bootstrap/js/dist/tab'
-import 'bootstrap/js/dist/collapse'
-import Turbolinks from "turbolinks"
+import "bootstrap/js/dist/modal";
+import "bootstrap/js/dist/tab";
+import "bootstrap/js/dist/collapse";
+import Turbolinks from "turbolinks";
 
-import hljs from 'highlight.js/lib/core'
-import _crystal from 'highlight.js/lib/languages/crystal'
-import _yaml from 'highlight.js/lib/languages/yaml'
-hljs.registerLanguage('crystal', _crystal)
-hljs.registerLanguage('yaml', _yaml)
-window.hljs = hljs
+import hljs from "highlight.js/lib/core";
+import _crystal from "highlight.js/lib/languages/crystal";
+import _yaml from "highlight.js/lib/languages/yaml";
+hljs.registerLanguage("crystal", _crystal);
+hljs.registerLanguage("yaml", _yaml);
+window.hljs = hljs;
 
-import 'jqcloud2/src/jqcloud'
+import "jqcloud2/src/jqcloud";
 
-import moveto from 'moveto/src/moveTo'
-window.MoveTo = moveto
+import moveto from "moveto/src/moveTo";
+window.MoveTo = moveto;
 
-import './js/jquery.twbsPagination.js'
-import './js/application.js'
+import "./js/jquery.twbsPagination.js";
+import "./js/application.js";
 
-Turbolinks.start()
+Turbolinks.start();
