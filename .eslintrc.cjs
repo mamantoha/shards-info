f04f280caafa663595c86199392929647fd82167
Anton Maminov <anton.linux@gmail.com>
2022-07-15 17:09:40 +0300

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  },
+  ignorePatterns: ['assets/vendor/*']
+}
