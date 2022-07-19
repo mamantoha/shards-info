817aa275837afa60282ffb0dd04fdad87adc28ea
Anton Maminov <anton.linux@gmail.com>
2022-07-19 19:50:16 +0300

    show tooltips for dependents repositories

--- assets/main.js
+++ assets/main.js
@@ -6,6 +6,7 @@ import 'bootstrap/js/dist/collapse'
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/alert'
 import 'bootstrap/js/dist/scrollspy'
+import 'bootstrap/js/dist/tooltip'
 
 import 'jqcloud2/src/jqcloud'
 
@@ -14,3 +15,5 @@ import moveto from 'moveto/src/moveTo'
 import './vendor/jquery.twbsPagination.js'
 import './js/application.js'
 window.MoveTo = moveto
+
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js')
