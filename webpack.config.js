5f806a58ba35a009d0a3af6eab9968a7343d93dd
Anton Maminov <anton.linux@gmail.com>
2022-10-07 17:18:39 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,44 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: [
-    './assets/main.js',
-    './assets/main.scss'
-  ],
+  mode: "production",
+  entry: ["./assets/main.js", "./assets/main.scss"],
   output: {
-    filename: 'application.js',
-    path: path.resolve(__dirname, 'public/dist')
+    filename: "application.js",
+    path: path.resolve(__dirname, "public/dist"),
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'application.css'
-    })
+      filename: "application.css",
+    }),
   ],
   module: {
     rules: [
       {
-        test: require.resolve('jquery'),
-        loader: 'expose-loader',
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
         options: {
-          exposes: ['$', 'jQuery']
-        }
+          exposes: ["$", "jQuery"],
+        },
       },
       {
         test: /\.(sa|sc|c)ss$/i,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options:
-          {
-            publicPath: '/dist/'
-          }
-          }, 'css-loader', 'sass-loader']
+            options: {
+              publicPath: "/dist/",
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(woff2?|svg|ttf|eot)$/,
-        type: 'asset/resource'
-      }
-    ]
-  }
-}
+        type: "asset/resource",
+      },
+    ],
+  },
+};
