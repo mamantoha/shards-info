3fbdf05e0b7a25c87c8405585214037e64ade457
Anton Maminov <anton.linux@gmail.com>
2022-11-20 15:14:03 +0200

    sync GitLab project languages

--- src/helpers/gitlab_helpers.cr
+++ src/helpers/gitlab_helpers.cr
@@ -237,4 +237,30 @@ module GitlabHelpers
       end
     end
   end
+
+  def sync_project_languages(repository : Repository)
+    gitlab_client = Gitlab::API.new(ENV["GITLAB_ACCESS_TOKEN"])
+
+    languages = gitlab_client.project_languages(repository.provider_id)
+
+    unlink_languages = repository.language_names - languages.keys
+
+    languages.each do |language_name, score|
+      language = Language.query.find_or_create(name: language_name)
+
+      repository_language =
+        RepositoryLanguage
+          .query
+          .find_or_build(repository_id: repository.id, language_id: language.id)
+
+      repository_language.score = score
+      repository_language.save!
+    end
+
+    unlink_languages.each do |language_name|
+      if (language = Language.query.find({name: language_name}))
+        repository.languages.unlink(language)
+      end
+    end
+  end
 end
