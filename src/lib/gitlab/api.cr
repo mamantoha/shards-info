247d1932c0afd0ac48436b65c7e6970f2721055d
Anton Maminov <anton.linux@gmail.com>
2022-11-30 11:11:37 +0200

    retry on 503: Service Unavailable

--- src/lib/gitlab/api.cr
+++ src/lib/gitlab/api.cr
@@ -47,7 +47,7 @@ module Gitlab
     end
 
     def make_request(url : String, params = {} of String => String)
-      Retriable.retry(on: {Crest::InternalServerError}) do
+      Retriable.retry(on: {Crest::InternalServerError, Crest::ServiceUnavailable}) do
         client[url].get(params: params)
       end
     ensure
