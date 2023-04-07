d366a2b6ce13e1455fa05d501cb26232bdde037c
Anton Maminov <anton.linux@gmail.com>
2023-04-07 17:54:44 +0300

    Update chart.js

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,42 +1,42 @@
 class RepositoryCountChart {
-  constructor(options) {
-    this.options = options;
-    this.element = document.querySelector(options.element);
-    this.chart = null;
-    this.startDateInput = document.createElement('input');
-    this.startDateInput.type = 'date';
-    this.element.appendChild(this.startDateInput);
-    this.endDateInput = document.createElement('input');
-    this.endDateInput.type = 'date';
-    this.element.appendChild(this.endDateInput);
-    this.startDateInput.addEventListener('change', this.updateChart.bind(this));
-    this.endDateInput.addEventListener('change', this.updateChart.bind(this));
-    this.fetchDataAndCreateChart();
+  constructor (options) {
+    this.options = options
+    this.element = document.querySelector(options.element)
+    this.chart = null
+    this.startDateInput = document.createElement('input')
+    this.startDateInput.type = 'date'
+    this.element.appendChild(this.startDateInput)
+    this.endDateInput = document.createElement('input')
+    this.endDateInput.type = 'date'
+    this.element.appendChild(this.endDateInput)
+    this.startDateInput.addEventListener('change', this.updateChart.bind(this))
+    this.endDateInput.addEventListener('change', this.updateChart.bind(this))
+    this.fetchDataAndCreateChart()
   }
 
-  fetchDataAndCreateChart() {
+  fetchDataAndCreateChart () {
     fetch(this.options.apiUrl)
       .then(response => response.json())
       .then(data => {
-        const chartData = this.processData(data);
-        const canvas = document.createElement('canvas');
-        this.element.appendChild(canvas);
+        const chartData = this.processData(data)
+        const canvas = document.createElement('canvas')
+        this.element.appendChild(canvas)
         this.chart = new Chart(canvas, {
           type: 'bar',
           data: chartData,
           options: this.options.chartOptions
-        });
+        })
       })
       .catch(error => {
-        console.error('Error fetching data:', error);
-      });
+        console.error('Error fetching data:', error)
+      })
   }
 
-  processData(data) {
-    const labels = Object.keys(data);
-    const values = Object.values(data);
+  processData (data) {
+    const labels = Object.keys(data)
+    const values = Object.values(data)
     const chartData = {
-      labels: labels,
+      labels,
       datasets: [{
         label: 'Repository Count',
         data: values,
@@ -44,41 +44,41 @@ class RepositoryCountChart {
         borderColor: 'rgba(54, 162, 235, 1)',
         borderWidth: 1
       }]
-    };
-    return chartData;
+    }
+    return chartData
   }
 
-  updateChart() {
-    const startDate = new Date(this.startDateInput.value);
-    const endDate = new Date(this.endDateInput.value);
+  updateChart () {
+    const startDate = new Date(this.startDateInput.value)
+    const endDate = new Date(this.endDateInput.value)
 
     fetch(this.options.apiUrl)
       .then(response => response.json())
       .then(data => {
-        const filteredData = this.filterDataByDateRange(data, startDate, endDate);
-        const chartData = this.processData(filteredData);
-        this.chart.data = chartData;
-        this.chart.update();
+        const filteredData = this.filterDataByDateRange(data, startDate, endDate)
+        const chartData = this.processData(filteredData)
+        this.chart.data = chartData
+        this.chart.update()
       })
       .catch(error => {
-        console.error('Error fetching data:', error);
-      });
+        console.error('Error fetching data:', error)
+      })
   }
 
-  filterDataByDateRange(data, startDate, endDate) {
+  filterDataByDateRange (data, startDate, endDate) {
     return Object.keys(data)
       .filter(date => {
-        const d = new Date(date);
-        return d >= startDate && d <= endDate;
+        const d = new Date(date)
+        return d >= startDate && d <= endDate
       })
       .reduce((obj, key) => {
-        obj[key] = data[key];
-        return obj;
-      }, {});
+        obj[key] = data[key]
+        return obj
+      }, {})
   }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const chart = new RepositoryCountChart({
     apiUrl: '/stats/created_at',
     element: '#chart-container',
@@ -91,5 +91,5 @@ document.addEventListener('DOMContentLoaded', function() {
         }]
       }
     }
-  });
-});
+  })
+})
