c4a116abc44d9e91facf674515e22f33dd5c13da
Anton Maminov <anton.linux@gmail.com>
2024-12-04 11:54:48 +0200

    fix typo

--- config/initializers/raven.cr
+++ config/initializers/raven.cr
@@ -6,5 +6,5 @@ Raven.configure do |config|
   config.current_environment = ENV.fetch("KEMAL_ENV", "development")
   config.connect_timeout = 5.seconds
   config.read_timeout = 5.seconds
-  config.excluded_exceptions = [HTTP::Server::ClientError]
+  config.excluded_exceptions = ["HTTP::Server::ClientError"]
 end
