8d54077ec8fdcfe952d899b1e4781bf3ddf84c2b
Anton Maminov <anton.linux@gmail.com>
2024-12-04 11:49:16 +0200

    exclude HTTP::Server::ClientError for sentry

--- config/initializers/raven.cr
+++ config/initializers/raven.cr
@@ -6,4 +6,5 @@ Raven.configure do |config|
   config.current_environment = ENV.fetch("KEMAL_ENV", "development")
   config.connect_timeout = 5.seconds
   config.read_timeout = 5.seconds
+  config.exluded_exceptions = [HTTP::Server::ClientError]
 end
