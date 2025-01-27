246489ebb646b01297ade5aa180c755944a3f2c7
Anton Maminov <anton.linux@gmail.com>
2025-01-27 14:16:45 +0200

    day -> days

--- config/initializers/kemal.cr
+++ config/initializers/kemal.cr
@@ -18,5 +18,5 @@ Kemal::Session.config do |config|
   config.cookie_name = "session_id"
   config.secret = ENV["KEMAL_SESSION_SECRET"]
   config.engine = Kemal::Session::RedisEngine.new(ENV["SESSION_REDIS_URL"])
-  config.timeout = 30.day
+  config.timeout = 30.days
 end
