737f430862cb3193d5fb7c21f08bcefc654e6ce2
Anton Maminov <anton.linux@gmail.com>
2025-03-12 17:35:01 +0200

    fix ameba warming

--- config/initializers/raven.cr
+++ config/initializers/raven.cr
@@ -2,7 +2,7 @@ require "raven"
 
 Raven.configure do |config|
   config.async = true
-  config.environments = %w(production)
+  config.environments = %w[production]
   config.current_environment = ENV.fetch("KEMAL_ENV", "development")
   config.connect_timeout = 5.seconds
   config.read_timeout = 5.seconds
