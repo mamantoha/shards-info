04f35443cc4414594787cd9902c34295c0557dc5
Anton Maminov <anton.linux@gmail.com>
2025-06-05 16:29:24 +0300

    extract RequestContext

---
+++ src/request_context.cr
@@ -0,0 +1,27 @@
+class RequestContext
+  property page_title : String = "shards.info"
+  property page_description : String = "View of all repositories on Github and Gitlab that have Crystal code in them"
+  property search_query : String = ""
+  property current_page : String = "home"
+  property open_graph : OpenGraph = OpenGraph.new
+
+  struct OpenGraph
+    property site_name, title, type, description, image, url
+
+    def initialize(
+      @site_name = "shards.info",
+      @title = "shards.info",
+      @type = "object",
+      @description = "View of all repositories on Github and Gitlab that have Crystal code in them",
+      @image = "https://shards.info/images/logo.png",
+      @url = "https://shards.info",
+    )
+    end
+  end
+end
+
+macro set_request_context(env, &)
+  request_context = {{env}}.get("request_context").as(RequestContext)
+  {{yield}}
+  {{env}}.set "request_context", request_context
+end
--- src/web.cr
+++ src/web.cr
@@ -23,38 +23,11 @@ require "raven/integrations/kemal"
 
 require "../config/config"
 require "./config"
+require "./request_context"
 require "./view_helpers"
 require "./delegators"
 
 require "./lib/cmark/readme_renderer"
-
-macro set_request_context(env, &)
-  request_context = {{env}}.get("request_context").as(RequestContext)
-  {{yield}}
-  {{env}}.set "request_context", request_context
-end
-
-class RequestContext
-  property page_title : String = "shards.info"
-  property page_description : String = "View of all repositories on Github and Gitlab that have Crystal code in them"
-  property search_query : String = ""
-  property current_page : String = "home"
-  property open_graph : OpenGraph = OpenGraph.new
-
-  struct OpenGraph
-    property site_name, title, type, description, image, url
-
-    def initialize(
-      @site_name = "shards.info",
-      @title = "shards.info",
-      @type = "object",
-      @description = "View of all repositories on Github and Gitlab that have Crystal code in them",
-      @image = "https://shards.info/images/logo.png",
-      @url = "https://shards.info",
-    )
-    end
-  end
-end
 
 add_context_storage_type(RequestContext)
 
