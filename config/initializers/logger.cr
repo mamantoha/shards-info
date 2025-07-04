10f63a224be2927281259cb5791253aefbb3d5b2
Anton Maminov <anton.linux@gmail.com>
2025-07-04 12:54:22 +0300

    add cache logger

--- config/initializers/logger.cr
+++ config/initializers/logger.cr
@@ -23,7 +23,17 @@ Log.setup do |c|
       STDOUT
     end
 
+  cache_log_file =
+    case ENV["KEMAL_ENV"]
+    when "production"
+      File.new("#{__DIR__}/../../log/cache.log", "a+")
+    else
+      STDOUT
+    end
+
+
   c.bind "kemal.*", :debug, Log::IOBackend.new(kemal_log_file)
   c.bind "clear.*", :debug, Log::IOBackend.new(clear_log_file)
   c.bind "mosquito.*", :debug, Log::IOBackend.new(mosquito_log_file)
+  c.bind "cache.*", :debug, Log::IOBackend.new(cache_log_file)
 end
