2f9d001cc923a502d12f5cc343de101934fb51ac
Anton Maminov <anton.linux@gmail.com>
2025-07-28 14:16:16 +0300

    add missing files

---
+++ src/active_users_tracker.cr
@@ -0,0 +1,29 @@
+class ActiveUserTracker
+  include HTTP::Handler
+
+  def initialize
+  end
+
+  def call(context : HTTP::Server::Context)
+    user_id = (context.request.cookies["user_id"]?.try(&.value) || UUID.random).to_s
+
+    context.response.cookies["user_id"] = user_id
+
+    remote_address = context.request.remote_address.try(&.to_s) || "0.0.0.0"
+
+    value = {
+      "remote_address" => remote_address,
+    }.to_json
+
+    ACTIVE_USERS_CACHE.write(user_id, value)
+
+    call_next(context)
+  end
+
+  def call(env)
+    user_id = env.request.cookies["user_id"]? || UUID.random.to_s
+    env.response.cookies["user_id"] = user_id
+
+    call_next env
+  end
+end
