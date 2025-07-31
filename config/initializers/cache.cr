b9ff38b79c6eb61b1bbc4ae5d10c046fc473cf59
Anton Maminov <anton.linux@gmail.com>
2025-07-31 11:59:12 +0300

    keep active users for 1 hour

--- config/initializers/cache.cr
+++ config/initializers/cache.cr
@@ -2,5 +2,5 @@ require "cache"
 require "redis_cache_store"
 
 CACHE              = Cache::RedisCacheStore(String, String).new(expires_in: 30.minutes)
-ACTIVE_USERS_CACHE = Cache::RedisCacheStore(String, String).new(expires_in: 5.minutes, namespace: "active-users")
+ACTIVE_USERS_CACHE = Cache::RedisCacheStore(String, String).new(expires_in: 1.hour, namespace: "active-users")
 IPAPI_CACHE        = Cache::RedisCacheStore(String, String).new(expires_in: 30.days, namespace: "ipapi")
