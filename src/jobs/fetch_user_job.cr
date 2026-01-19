80814eed423c1c71938ef3bffe106adef3f6c72d
Anton Maminov <anton.linux@gmail.com>
2026-01-19 13:06:03 +0200

    Update fetch_user_job.cr

--- src/jobs/fetch_user_job.cr
+++ src/jobs/fetch_user_job.cr
@@ -2,13 +2,13 @@ class FetchUserJob < MosquitoQueuedJobWithErrorHandler
   param user_id : Int64
 
   def perform
-    user = User.find! user_id
-
-    case user.provider
-    when "github"
-      GithubHelpers.resync_user user
-    when "gitlab"
-      GitlabHelpers.resync_user user
+    if user = User.find(user_id)
+      case user.provider
+      when "github"
+        GithubHelpers.resync_user user
+      when "gitlab"
+        GitlabHelpers.resync_user user
+      end
     end
   end
 end
