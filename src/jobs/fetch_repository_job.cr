59b3ddfb9c09cd8e44f8dab0369a29d77b238a0c
Anton Maminov <anton.linux@gmail.com>
2026-01-19 13:05:22 +0200

    Update fetch_repository_job.cr

--- src/jobs/fetch_repository_job.cr
+++ src/jobs/fetch_repository_job.cr
@@ -2,13 +2,13 @@ class FetchRepositoryJob < MosquitoQueuedJobWithErrorHandler
   param repository_id : Int64
 
   def perform
-    repository = Repository.find! repository_id
-
-    case repository.provider
-    when "github"
-      GithubHelpers.resync_repository repository
-    when "gitlab"
-      GitlabHelpers.resync_repository repository
+    if repository = Repository.find(repository_id)
+      case repository.provider
+      when "github"
+        GithubHelpers.resync_repository repository
+      when "gitlab"
+        GitlabHelpers.resync_repository repository
+      end
     end
   end
 end
