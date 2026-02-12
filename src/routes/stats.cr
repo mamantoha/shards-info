4c8b9de05da8d8921bb995bfd2572c4fc62fd11e
Anton Maminov <anton.linux@gmail.com>
2026-02-12 15:23:56 +0200

    fix ameba warnings

--- src/routes/stats.cr
+++ src/routes/stats.cr
@@ -5,7 +5,7 @@ router.namespace "/stats" do
     env.response.content_type = "application/json"
   end
 
-  get "/created_at" do |env|
+  get "/created_at" do
     CACHE.fetch("stats:created_at", expires_in: 2.hours) do
       repositiries =
         Repository
@@ -27,7 +27,7 @@ router.namespace "/stats" do
     end
   end
 
-  get "/last_activity_at" do |env|
+  get "/last_activity_at" do
     CACHE.fetch("stats:last_activity_at_json", expires_in: 2.hours) do
       repositiries =
         Repository
@@ -49,7 +49,7 @@ router.namespace "/stats" do
     end
   end
 
-  get "/repositories_growth" do |env|
+  get "/repositories_growth" do
     # Calculates the cumulative count of repositories created before and on each year-month date,
     # including months with no repositories
     CACHE.fetch("stats:repositories_growth", expires_in: 2.hours) do
@@ -59,12 +59,12 @@ router.namespace "/stats" do
       date_start = "2012-11-01"
 
       month_series = Lustra::SQL.select(<<-SQL
-      generate_series(
-        '#{date_start}'::date,
-        (SELECT date_trunc('month', MAX(created_at)) FROM repositories),
-        '1 month'::interval
-      )::date AS year_month
-      SQL
+        generate_series(
+          '#{date_start}'::date,
+          (SELECT date_trunc('month', MAX(created_at)) FROM repositories),
+          '1 month'::interval
+        )::date AS year_month
+        SQL
       )
 
       Lustra::SQL
@@ -84,7 +84,7 @@ router.namespace "/stats" do
   end
 
   # Number of direct dependencies
-  get "/direct_dependencies" do |env|
+  get "/direct_dependencies" do
     CACHE.fetch("stats:direct_dependencies", expires_in: 2.hours) do
       hsh = {} of Int64 => Int64
 
@@ -94,18 +94,18 @@ router.namespace "/stats" do
           "COUNT(*) AS repository_count"
         )
         .from(<<-SQL
-        (
-          SELECT
-            r.id,
-            COUNT(rel.id) AS dependency_count
-          FROM
-            repositories r
-          LEFT JOIN
-            relationships rel ON r.id = rel.master_id
-          GROUP BY
-            r.id
-        ) AS repo_dependency_count
-        SQL
+          (
+            SELECT
+              r.id,
+              COUNT(rel.id) AS dependency_count
+            FROM
+              repositories r
+            LEFT JOIN
+              relationships rel ON r.id = rel.master_id
+            GROUP BY
+              r.id
+          ) AS repo_dependency_count
+          SQL
         )
         .group_by("dependency_count")
         .order_by("dependency_count", :asc)
@@ -118,7 +118,7 @@ router.namespace "/stats" do
   end
 
   # Number of transitive reverse dependencies
-  get "/reverse_dependencies" do |env|
+  get "/reverse_dependencies" do
     CACHE.fetch("stats:reverse_dependencies", expires_in: 2.hours) do
       hsh = {} of String => Int64
 
@@ -128,7 +128,7 @@ router.namespace "/stats" do
           WHEN dependency_count BETWEEN 30 AND 100 THEN CONCAT((dependency_count / 10) * 10, '-', (dependency_count / 10) * 10 + 9)
           ELSE CONCAT((dependency_count / 100) * 100, '-', (dependency_count / 100) * 100 + 99)
         END AS dependency_range
-      SQL
+        SQL
 
       Lustra::SQL
         .select(
@@ -136,18 +136,18 @@ router.namespace "/stats" do
           "COUNT(*) AS repository_count"
         )
         .from(<<-SQL
-        (
-          SELECT
-            r.id,
-            COUNT(rel.id) AS dependency_count
-          FROM
-            repositories r
-          LEFT JOIN
-            relationships rel ON r.id = rel.dependency_id
-          GROUP BY
-            r.id
-        ) AS repo_dependency_count
-        SQL
+          (
+            SELECT
+              r.id,
+              COUNT(rel.id) AS dependency_count
+            FROM
+              repositories r
+            LEFT JOIN
+              relationships rel ON r.id = rel.dependency_id
+            GROUP BY
+              r.id
+          ) AS repo_dependency_count
+          SQL
         )
         .group_by("dependency_range")
         .order_by("MIN(dependency_count)")
@@ -160,7 +160,7 @@ router.namespace "/stats" do
     end
   end
 
-  get "/user_repositories_count" do |env|
+  get "/user_repositories_count" do
     CACHE.fetch("stats:user_repositories_count", expires_in: 2.hours) do
       hsh = {} of Int64 => Int64
 
@@ -170,16 +170,16 @@ router.namespace "/stats" do
           "COUNT(*) AS user_count"
         )
         .from(<<-SQL
-        (
-          SELECT
-            user_id,
-            COUNT(*) AS repo_count
-          FROM
-            repositories
-          GROUP BY
-            user_id
-        ) AS user_repos
-        SQL
+          (
+            SELECT
+              user_id,
+              COUNT(*) AS repo_count
+            FROM
+              repositories
+            GROUP BY
+              user_id
+          ) AS user_repos
+          SQL
         )
         .group_by("repo_count")
         .order_by("repo_count")
@@ -191,7 +191,7 @@ router.namespace "/stats" do
     end
   end
 
-  get "/repositories_provider_count" do |env|
+  get "/repositories_provider_count" do
     CACHE.fetch("stats:repositories_provider_count", expires_in: 2.hours) do
       hsh = {} of String => Int64
 
@@ -208,7 +208,7 @@ router.namespace "/stats" do
     end
   end
 
-  get "/users_provider_count" do |env|
+  get "/users_provider_count" do
     CACHE.fetch("stats:users_provider_count", expires_in: 2.hours) do
       hsh = {} of String => Int64
 
