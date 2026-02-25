9f80c45dc5463735134c132bbe04e0f9f6feb9fc
Anton Maminov <anton.linux@gmail.com>
2026-02-25 15:14:32 +0200

    temporary disable throttle

--- config/initializers/defense.cr
+++ config/initializers/defense.cr
@@ -6,29 +6,29 @@ Defense.store = if ENV["DEFENSE_REDIS_URL"]?.nil?
                   Defense::RedisStore.new(url: ENV["DEFENSE_REDIS_URL"])
                 end
 
-Defense.throttle("throttle requests per minute", limit: 60, period: 60) do |request|
-  next if request.headers["X-Requested-With"]? == "XMLHttpRequest"
+# Defense.throttle("throttle requests per minute", limit: 60, period: 60) do |request|
+#   next if request.headers["X-Requested-With"]? == "XMLHttpRequest"
 
-  Helpers.real_ip(request)
-end
+#   Helpers.real_ip(request)
+# end
 
-Defense.throttled_response = ->(response : HTTP::Server::Response) do
-  response.status = HTTP::Status::TOO_MANY_REQUESTS
-  response.content_type = "text/html"
-  response.headers.add "Retry-After", "60"
+# Defense.throttled_response = ->(response : HTTP::Server::Response) do
+#   response.status = HTTP::Status::TOO_MANY_REQUESTS
+#   response.content_type = "text/html"
+#   response.headers.add "Retry-After", "60"
 
-  response.print(<<-HTML)
-    <html>
-      <head>
-        <title>Too Many Requests</title>
-      </head>
-      <body>
-        <h1>Too Many Requests</h1>
-        <p>You're doing that too often! Try again later.</p>
-      </body>
-    </html>
-    HTML
-end
+#   response.print(<<-HTML)
+#     <html>
+#       <head>
+#         <title>Too Many Requests</title>
+#       </head>
+#       <body>
+#         <h1>Too Many Requests</h1>
+#         <p>You're doing that too often! Try again later.</p>
+#       </body>
+#     </html>
+#     HTML
+# end
 
 Defense.blocklist("fail2ban pentesters") do |request|
   Defense::Fail2Ban.filter("pentesters:#{Helpers.real_ip(request)}", maxretry: 5, findtime: 60, bantime: 24 * 60 * 60) do
