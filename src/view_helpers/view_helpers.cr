97d84eef5ce05f4e284eb3b7955c98af00a765c9
Anton Maminov <anton.linux@gmail.com>
2026-09-09 11:47:17 +0300

    Extract local datetime view helper

--- src/view_helpers/view_helpers.cr
+++ src/view_helpers/view_helpers.cr
@@ -1,6 +1,12 @@
 require "ecr"
 
 module ViewHelpers
+  extend self
+
+  def local_datetime(time : Time, format = "%Y-%m-%d %H:%M:%S") : String
+    %(<time class="js-local-datetime" datetime="#{time.to_rfc3339}">#{time.to_s(format)}</time>)
+  end
+
   record Paginator, current_page : Int32, per_page : Int32, total_count : Int64, link : String do
     ECR.def_to_s "#{__DIR__}/paginate.ecr"
   end
